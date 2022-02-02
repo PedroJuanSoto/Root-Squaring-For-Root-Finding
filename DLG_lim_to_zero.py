9e002796cf1da87aec4e30fe55589e40130a966c
psoto004@fiu.edu <psoto@gradcenter.cuny.edu>
2022-02-01 19:09:04 -0500

    Added a test to see if lim to 0 from random directions works, l = 6 is the trouble spot

---
+++ DLG_lim_to_zero.py
@@ -0,0 +1,29 @@
+from DLG_alg_mpmath import DLG_rational_form, DLG, add, sub, mul, div, precision, mpc, mpf
+import mpmath as mp
+
+#p is a black box polynomial and p' is its derivative.
+#x = r*e^{2*pi*i*(t/u)}, and l is the number of derivatives/depth of recursion
+p  = lambda x:(x**2-4)*(x**2+4)
+dp  = lambda x:4*x*(x**2)
+
+#l_max is the maximum l we will try and log_epsilon_max is the -log_2 of the
+#smallest epsilon to zero that we will try
+l_max           = 10
+log_epsilon_max = 10
+
+#x is the point which defines a line to 0 on which we are taking a limit
+angle = mp.rand()
+x = mp.expjpi(angle*2)
+
+#We want to "test" if the limit to 0 makes sense, to do this we check whether
+#the sequence to zero to "cauchy-like" by checking if differences between points
+#in the sequnce get smaller
+new  = mpc(0,0)
+prev = mpc(0,0)
+for l in range(l_max):
+    precision += 2
+    mp.mp.dps = precision
+    for e in range(1,20):
+        new = DLG(p,dp,sub(0,mul(x,mp.power(2,-e))),l)
+        print(l,e,mp.fabs(sub(new,prev)))
+        prev = new
