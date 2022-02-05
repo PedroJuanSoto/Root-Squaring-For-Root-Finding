6c8e253a968eef0a4864650baa83efbd3406cc69
PedroJuanSoto <psoto@gradcenter.cuny.edu>
2022-02-05 11:33:04 -0500

    Fixed it so that it does both bounds now

--- root_radius_test_mpmath.py
+++ root_radius_test_mpmath.py
@@ -33,14 +33,14 @@ for l in range(l_max):
         print("radius=", float(mp.fabs(real)))
         print("error=", mp.fabs(int(mp.fabs(real))-int(mp.fabs(approx)))/int(mp.fabs(real)))
 
-# for l in range(l_max):
-#     precision *= 2
-#     mp.mp.dps = precision
-#     print("l=",l)
-#     for e in range(1,20):
-#         print("e=", e)
-#         approx = div(DLG(p_rev,dp_rev,sub(0,mul(x,mp.power(2,-e))),l),div(1,d))
-#         print("approx=",float(mp.fabs(approx)))
-#         real = mp.power(2,mp.power(2,l))
-#         print("radius=", float(mp.fabs(real)))
-#         print("error=", mp.fabs(int(mp.fabs(real))-int(mp.fabs(approx)))/int(mp.fabs(real)))
+for l in range(l_max):
+    precision *= 2
+    mp.mp.dps = precision
+    print("l=",l)
+    for e in range(1,20):
+        print("e=", e)
+        approx = div(DLG(p_rev,dp_rev,sub(0,mul(x,mp.power(2,-e))),l),d)
+        print("approx=",float(mp.fabs(approx)))
+        real = mp.power(2,mp.power(2,l))
+        print("radius=", float(mp.fabs(real)))
+        print("error=", mp.fabs(int(mp.fabs(real))-int(mp.fabs(approx)))/int(mp.fabs(real)))
