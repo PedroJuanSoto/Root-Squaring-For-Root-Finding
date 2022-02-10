bc0f723190003b48b07131c00e150db5428e7b54
PedroJuanSoto <psoto@gradcenter.cuny.edu>
2022-02-09 21:38:06 -0500

    Improved the precision

--- random_root_poly_gen.py
+++ random_root_poly_gen.py
@@ -15,19 +15,11 @@ degree                 =  2**log_d
 #uniform random
 gen_rand = lambda low, high : mpf((high - low)*mp.rand() + low)
 
-import cmath
-import random
-import numpy as np
-
-
 #Generates random complex numbers in the range low,high
 def gen_c_num():
-	a = random.uniform(low, high)
-	b = random.uniform(low, high)
+	a = gen_rand(low, high)
+	b = gen_rand(low, high)
 	return complex(a,b)
-
-#converts numpy to mpmath
-np_to_mpc = lambda z : mpc(z.real,z.imag)
 
 #Generates a random ploynomial of degree d whose roots are generated uniformly randomly
 def gen_rand_poly(d):
@@ -36,35 +28,29 @@ def gen_rand_poly(d):
 		roots.append(gen_c_num())
 	linear_factors = []
 	for r in roots:
-		linear_factors.append(np.poly1d([1,-r]))
-	poly = np.poly1d([1])
+		linear_factors.append([1,-r])
+	poly = linear_factors.pop()
+	print(poly)
 	for p in linear_factors:
-		poly *= p
-	coeffs = list(map(np_to_mpc,list(poly)))
+		poly = mul_poly_lin_factor(poly,p)
+	coeffs = poly
 	coeffs_rev = deepcopy(coeffs)
 	coeffs.reverse()
 	def p_rev(x):
 		return eval(coeffs,x)
-		# result = mpc(0,0)
-		# for i,c in enumerate(coeffs):
-		# 	result = add(result,mul(c,pod(x,i)))
-		# return result
 	def dp_rev(x):
 		return eval_deriv(coeffs,x)
-		# result = mpc(0,0)
-		# for i in range(1,len(coeffs)):
-		# 	result = add(result,mul(mul(i,coeffs[i]),pod(x,i-1)))
-		# return result
 	def p(x):
 		return eval(coeffs_rev,x)
-		# result = mpc(0,0)
-		# for i,c in enumerate(coeffs_rev):
-		# 	result = add(result,mul(c,pod(x,i)))
-		# return result
 	def dp(x):
 		return eval_deriv(coeffs_rev,x)
-		# result = mpc(0,0)
-		# for i in range(1,len(coeffs_rev)):
-		# 	result = add(result,mul(mul(i,coeffs_rev[i]),pod(x,i-1)))
-		# return result
 	return p, dp, p_rev, dp_rev, roots
+
+
+def mul_poly_lin_factor(poly,lin):
+	left = deepcopy(poly)
+	left.append(0)
+	out = [poly[0]]
+	for i in range(len(poly)):
+		out.append(add(left[i+1],mul(lin[1],poly[i])))
+	return out
